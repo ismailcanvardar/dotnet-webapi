4b260b0fb34cf6ff3b2f7226d00835c23415e199
mythi <ismailcanvardar@gmail.com>
2020-10-25 16:27:54 +0300

    missed interface added

---
+++ Data/IAboutEmployeeRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using KariyerAppApi.Models;
+
+namespace KariyerAppApi.Data
+{
+    public interface IAboutEmployeeRepo
+    {
+        AboutEmployee GetAboutEmployee(Guid employeeId);
+        void CreateAboutEmployee(AboutEmployee aboutEmployee);
+        bool SaveChanges();
+    }
+}
